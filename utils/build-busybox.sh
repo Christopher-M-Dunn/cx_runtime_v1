381e11597920e3b9a14c4eb2ff1ec19af6591619
Christopher-M-Dunn <christopher.m.dunn@gmail.com>
2026-03-24 00:12:45 +0000

    replaced broken build-busybox.sh with the one from V2

--- utils/build-busybox.sh
+++ utils/build-busybox.sh
@@ -1,25 +1,46 @@
 # Builds busybox and the filesystem needed for linux on qemu
 # Taken from: https://risc-v-machines.readthedocs.io/en/latest/linux/simple/
 
+# Make sure to set the build to static:
+# Busybox Settings --->
+# Build Options --->
+# Build BusyBox as a static binary (no shared libs) ---> yes
+
+
 git clone https://git.busybox.net/busybox
 pushd busybox
-git checkout 1_32_1
+git checkout 1_32_stable
 
 make ARCH=riscv CROSS_COMPILE=riscv32-unknown-linux-gnu- defconfig
-make ARCH=riscv CROSS_COMPILE=riscv32-unknown-linux-gnu- menuconfig
-make ARCH=riscv CROSS_COMPILE=riscv64-unknown-linux-gnu- -j $(nproc)
+# make ARCH=riscv CROSS_COMPILE=riscv32-unknown-linux-gnu- menuconfig
 
+sed -i 's/# CONFIG_STATIC is not set/CONFIG_STATIC=y/g' .config
+
+# LONG_BIT is not always found
+sed -i 's/LONG_BIT/32/g' networking/tls_aesgcm.c
+
+make ARCH=riscv CROSS_COMPILE=riscv32-unknown-linux-gnu- -j $(nproc)
+
+popd
 mkdir initramfs
 pushd initramfs
 mkdir -p {bin,sbin,dev,etc,home,mnt,proc,sys,usr,tmp}
 mkdir -p usr/{bin,sbin}
 mkdir -p proc/sys/kernel
 pushd dev
-sudo mknod sda b 8 0 
+sudo mknod sda b 8 0
 sudo mknod console c 5 1
 popd
 
 cp ../busybox/busybox ./bin/
+
+if [ $? -ne 0 ]; then
+    echo "Couldn't find busybox executeable - maybe it was build incorrectly?"
+    exit 1
+fi
+
+#~ Delete init if it exists, otherwise do nothing
+rm -f init
 
 touch init
 
